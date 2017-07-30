6893dc69d4a5626d34cdd0bbdb3c2cbe6b8f585f
Phillip Troutman <philliptroutman21@gmail.com>
2017-07-30 14:57:45 -0700

    not sure what im doing

---
+++ src/js/popup.js
@@ -0,0 +1,9 @@
+$("button").on("click",function(tab){
+
+
+
+  // fist try on changing the background color.
+  //chrome.tabs.executeScript({
+  //   code: "document.body.style.backgroundColor='red'"
+  // });
+});
