41c9ffc11e87c14c79d4f18ab2e262d490678fc8
Phillip Troutman <philliptroutman21@gmail.com>
2017-07-31 21:09:19 -0700

    big change

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,8 +1,5 @@
 $("button").on("click",function(tab){
-  
 
-  // fist try on changing the background color.
-  //chrome.tabs.executeScript({
-  //   code: "document.body.style.backgroundColor='red'"
-  // });
+
+
 });
